09c2fe69e497ac3aafb9352c8326b07fe386baf8
nichihachi <kevin.angelier@aol.fr>
2024-12-15 13:50:14 +0100

    feature(CineTour): backend frontend - Init project

---
+++ backend/src/test/java/com/Polytech/CineTour/CineTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Polytech.CineTour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
