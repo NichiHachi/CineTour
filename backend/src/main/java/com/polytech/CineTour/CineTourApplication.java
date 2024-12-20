f9ffb5afadbaa775542271fd071f5eedd535ae3c
nichihachi <kevin.angelier@aol.fr>
2024-12-20 14:24:22 +0100

    tweak(backend): Backend gradle

--- backend/src/main/java/com/polytech/CineTour/CineTourApplication.java
+++ backend/src/main/java/com/polytech/CineTour/CineTourApplication.java
@@ -1,4 +1,4 @@
-package com.Polytech.CineTour;
+package com.polytech.CineTour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/polytech/CineTour/CineTourApplicationTests.java
+++ backend/src/test/java/com/polytech/CineTour/CineTourApplicationTests.java
@@ -1,4 +1,4 @@
-package com.Polytech.CineTour;
+package com.polytech.CineTour;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
