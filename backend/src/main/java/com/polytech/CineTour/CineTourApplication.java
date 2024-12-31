e4f2d5dcbca09845de859a8410cb110b719a8ae7
Morph01 <145839520+Morph01@users.noreply.github.com>
2024-12-31 22:49:26 +0100

    feat(import): add import functionality for IMDb movies with profile-based execution and logging configuration

--- backend/src/main/java/com/polytech/CineTour/CineTourApplication.java
+++ backend/src/main/java/com/polytech/CineTour/CineTourApplication.java
@@ -1,5 +1,8 @@
 package com.polytech.CineTour;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,29 +11,52 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.polytech.db.DataConnection;
+import com.polytech.crud.service.ImdbMoviesService;
+import com.polytech.utils.Console;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.polytech.crud.entity")
 @EnableJpaRepositories(basePackages = "com.polytech.crud.repository")
-@ComponentScan(basePackages = {"com.polytech.crud.controller", "com.polytech.crud.service"})
+@ComponentScan(basePackages = {
+        "com.polytech.crud.controller",
+        "com.polytech.crud.service",
+})
 @RestController
-public class CineTourApplication {
+public class CineTourApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CineTourApplication.class, args);
+    @Autowired
+    private ImdbMoviesService imdbMovies;
 
-		DataConnection dataConnection = new DataConnection();
-		dataConnection.connect();
-		dataConnection.executeQuery("SHOW DATABASES;");
-		dataConnection.executeQuery("SHOW TABLES;");
-		dataConnection.executeQuery("SELECT * FROM movies;");
-		dataConnection.disconnect();
-	}
+    @Value("${spring.profiles.active:default}")
+    private String activeProfile;
 
-	@GetMapping
-	public String hello(){
-		return "Hello World";
-	}
+    @PostConstruct
+    public void init() {
+        Console.warnln("Active profile: " + activeProfile + "\n");
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(CineTourApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if ("import".equals(activeProfile)) {
+            Console.warnln("Import mode activated\n");
+            String gzFilePath = "title.basics.tsv.gz";
+            String tsvFilePath = "title.basics.tsv";
+            imdbMovies.importMovies(gzFilePath, tsvFilePath);
+            Console.warnln("Import finished, restart application with default profile if needed\n");
+            System.exit(0); // Clean exit after import
+        } else {
+            Console.warnln("Default mode activated\n");
+        }
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
 }
