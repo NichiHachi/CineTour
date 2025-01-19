d9420056df1d0f3815cacc14320e5d36822e4420
Morph01 <145839520+Morph01@users.noreply.github.com>
2025-01-19 13:28:17 +0100

    fix: simplify backend api url and remove crossorigin annotation

--- backend/src/main/java/com/polytech/crud/controller/UserController.java
+++ backend/src/main/java/com/polytech/crud/controller/UserController.java
@@ -3,7 +3,6 @@ package com.polytech.crud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +17,6 @@ import com.polytech.crud.service.UserService;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @Autowired
     private UserService service;
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,9 @@
 // frontend/src/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:9001'; 
-
 export const getAllUsers = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/users/all`);
+        const response = await axios.get(`/users/all`);
         return response.data;
     } catch (error) {
         console.error('Error fetching users:', error);
@@ -15,7 +13,7 @@ export const getAllUsers = async () => {
 
 export const getUserById = async (id) => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/users/${id}`);
+        const response = await axios.get(`/users/${id}`);
         return response.data;
     } catch (error) {
         console.error('Error fetching user by ID:', error);
@@ -26,7 +24,7 @@ export const getUserById = async (id) => {
 export const addUser = async (user) => {
     try {
         console.log('user:', user);
-        const response = await axios.post(`${API_BASE_URL}/users/add`, user);
+        const response = await axios.post(`/users/add`, user);
         return response.data;
     } catch (error) {
         console.error('Error adding user:', error);
