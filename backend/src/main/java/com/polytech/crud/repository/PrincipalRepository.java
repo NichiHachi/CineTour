199e418c6655cbeaee97a10c5ed3152e13ff53eb
Morph01 <thibaut6969delastreet@gmail.com>
2026-01-15 20:36:11 +0100

    refactor: update method name and remove deprecated dependencies

--- backend/src/main/java/com/polytech/crud/repository/PrincipalRepository.java
+++ backend/src/main/java/com/polytech/crud/repository/PrincipalRepository.java
@@ -5,5 +5,5 @@ import com.polytech.crud.entity.Principal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PrincipalRepository extends JpaRepository<Principal, Long> {
-    Principal findByIdImdbNconstOrdering(String idImdb, String nconst, Integer ordering);
+    Principal findByIdImdbAndNconstAndOrdering(String idImdb, String nconst, Integer ordering);
 }
