4aa2f7ed12bd5d2362b7c436935f3b1499a411eb
Morph01 <thibaut6969delastreet@gmail.com>
2026-01-15 21:12:51 +0100

    feat: add ratings importation

--- backend/src/main/java/com/polytech/CineTour/CineTourApplication.java
+++ backend/src/main/java/com/polytech/CineTour/CineTourApplication.java
@@ -2,8 +2,8 @@ package com.polytech.CineTour;
 
 import java.util.List;
 
-import com.polytech.crud.entity.Director;
-import com.polytech.crud.service.ImdbDirectorsService;
+import com.polytech.crud.entity.*;
+import com.polytech.crud.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -15,13 +15,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.polytech.crud.entity.Movie;
-import com.polytech.crud.entity.Person;
-import com.polytech.crud.entity.Principal;
-import com.polytech.crud.service.ImdbLocationsService;
-import com.polytech.crud.service.ImdbMoviesService;
-import com.polytech.crud.service.ImdbPersonService;
-import com.polytech.crud.service.ImdbPrincipalsService;
 import com.polytech.utils.Console;
 
 import jakarta.annotation.PostConstruct;
@@ -51,6 +44,9 @@ public class CineTourApplication implements CommandLineRunner {
     @Autowired
     private ImdbPrincipalsService imdbPrincipals;
 
+    @Autowired
+    private ImdbRatingsService imdbRatings;
+
     @Value("${spring.profiles.active:default}")
     private String activeProfile;
 
@@ -68,7 +64,7 @@ public class CineTourApplication implements CommandLineRunner {
      * If the active profile is “import”, then first import the films and then
      * switch to the default state.
      * Otherwise, the default profile is “default”.
-     * 
+     *
      * @param args
      * @throws Exception
      */
@@ -103,6 +99,12 @@ public class CineTourApplication implements CommandLineRunner {
             List<Principal> principals = imdbPrincipals.getPrincipals();
             imdbPrincipals.importPrincipals(principals);
             Console.warnln(principals.size() + " Principals imported\n");
+
+            // Ratings
+            Console.warnln("Ratings importation started");
+            List<Rating> ratings = imdbRatings.getRatings();
+            imdbRatings.importRatings(ratings);
+            Console.warnln(ratings.size() + " Ratings imported\n");
 
             System.exit(0); // Clean exit after import
         } else {
---
+++ backend/src/main/java/com/polytech/crud/service/ImdbRatingsService.java
@@ -0,0 +1,92 @@
+package com.polytech.crud.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.polytech.crud.entity.Rating;
+import com.polytech.crud.repository.RatingRepository;
+import com.polytech.utils.ImdbDatasets;
+
+import jakarta.persistence.EntityManager;
+
+@Service
+public class ImdbRatingsService {
+    @Autowired
+    private RatingRepository ratingRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    private List<Rating> parseRatingsTsvFile(String tsvFilePath) throws IOException {
+        List<Rating> ratings = new ArrayList<>();
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(tsvFilePath))) {
+            // Skip header
+            String line = reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                try {
+                    String[] fields = line.split("\t");
+                    Rating rating = new Rating();
+                    rating.setIdImdb(fields[0]);
+                    if (!fields[1].equals("\\N")) {
+                        rating.setAverageRating(Double.valueOf(fields[1]));
+                    } else {
+                        rating.setAverageRating(null);
+                    }
+                    if (!fields[2].equals("\\N")) {
+                        rating.setNumVotes(Integer.valueOf(fields[2]));
+                    } else {
+                        rating.setNumVotes(Integer.valueOf(fields[2]));
+                    }
+                    ratings.add(rating);
+
+                } catch (Exception e) {
+                    System.err.println("Skipping malformed line: " + line);
+                }
+            }
+        }
+        return ratings;
+    }
+
+    public List<Rating> getRatings() throws IOException {
+        String gzFileName = ImdbDatasets.RATINGS.getFileName();
+        String tsvFileName = gzFileName.replace(".gz", "");
+
+        try {
+            ImdbExtraction.downloadFile(ImdbDatasets.RATINGS.getUrl(), gzFileName);
+        } catch (Exception e) {
+            System.out.println("Failed to download file: " + e.getMessage());
+            return new ArrayList<>();
+        }
+        System.out.println("Extracting IMDb dataset");
+        ImdbExtraction.extractGzFile(gzFileName);
+        System.out.println("Parsing IMDb dataset");
+        return parseRatingsTsvFile(tsvFileName);
+    }
+
+    public void importRatings(List<Rating> ratings) {
+        System.out.println("Saving ratings to database");
+
+        int batchSize = 10000;
+
+        for (int i = 0; i < ratings.size(); i += batchSize) {
+            int end = Math.min(i + batchSize, ratings.size());
+            List<Rating> batch = ratings.subList(i, end);
+
+            ratingRepository.saveAll(batch);
+            entityManager.flush();
+            entityManager.clear();
+
+            System.out.println("Saved " + end + " / " + ratings.size() + " ratings...");
+        }
+
+        System.out.println("Finished importing ratings");
+    }
+}
