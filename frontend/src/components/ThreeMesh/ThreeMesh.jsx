14d01f3ca5acbc5adabc59eea79b539ecaa2e070
Xeon0X <xeon0x.gaming@gmail.com>
2026-01-20 23:52:00 +0100

    fix(front): dynamic color theme on globe

--- frontend/src/components/ThreeMesh/ThreeMesh.jsx
+++ frontend/src/components/ThreeMesh/ThreeMesh.jsx
@@ -1,21 +1,42 @@
 // three-mesh.js
 
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 const ThreeMesh = () => {
   const [color, setColor] = useState("#000");
+  const colorRef = useRef(null);
 
+  // Update color when prefers-color-scheme changes
   useEffect(() => {
-    const cssColor = getComputedStyle(document.documentElement)
-      .getPropertyValue("--color-bg-light")
-      .trim();
-    setColor(cssColor);
+    const updateColor = () => {
+      const cssColor = getComputedStyle(document.documentElement)
+        .getPropertyValue("--color-bg-light")
+        .trim();
+      setColor(cssColor);
+    };
+
+    const mq = window.matchMedia("(prefers-color-scheme: dark)");
+
+    updateColor();
+    mq.addEventListener("change", updateColor);
+
+    return () => mq.removeEventListener("change", updateColor);
   }, []);
+
+  // Update Three.js lights when color changes
+  useEffect(() => {
+    if (colorRef.current) colorRef.current.color.set(color);
+  }, [color]);
 
   return (
     <mesh>
       <sphereGeometry args={[1, 64, 64]} />
-      <meshPhongMaterial color={color} transparent={false} opacity={0.1} />
+      <meshPhongMaterial
+        color={color}
+        transparent={false}
+        opacity={0.1}
+        ref={colorRef}
+      />
     </mesh>
   );
 };
