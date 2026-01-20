dfb25334bb6d89370f7533924659159b9e75ae10
Morph01 <thibaut6969delastreet@gmail.com>
2026-01-21 00:21:54 +0100

    fix: xmx to 8g

--- backend/start.sh
+++ backend/start.sh
@@ -2,4 +2,4 @@
 
 gradle --version
 gradle build --continuous -x test &
-gradle bootRun -Dspring-boot.run.jvmArguments="-Dspring.devtools.restart.enabled=true -Djava.net.preferIPv4Stack=true"
+gradle bootRun -Dspring-boot.run.jvmArguments="$JAVA_OPTS -Dspring.devtools.restart.enabled=true -Djava.net.preferIPv4Stack=true"
